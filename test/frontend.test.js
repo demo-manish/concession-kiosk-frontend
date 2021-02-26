66cf50511f2ada6f16fe4bdccf44502414503013
demo-manish <79218105+demo-manish@users.noreply.github.com>
2021-02-25 21:34:48 -0500

    hjkh

--- test/frontend.test.js
+++ test/frontend.test.js
@@ -1,9 +1,9 @@
-const expect = require('chai').expect;
+const assert = require('assert');
 describe('Simple Math Test', () => {
  it('should return 2', () => {
-        expect(1 + 1).to.equal(2);
+        assert.equal(1 + 1, 2);
     });
  it('should return 9', () => {
-        expect(3 * 3).to.equal(9);
+        assert.equal(3 * 2, 9);
     });
 });
